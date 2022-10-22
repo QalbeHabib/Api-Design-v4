6889548e7df28054e45b772fb6f18bdd4fe603bb
Qalb E Habib <rana.habib313@gmail.com>
2022-10-22 22:10:27 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+
+app('get' , (req ,res)=>{
+    res.send('habb from the server')
+})
+
+
+app.listen('4000' , ()=>{
+    console.log('server is runnign on the port 4000')
+})
