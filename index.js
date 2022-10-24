b5b758559e2214905c8f093228fa2fc4659ecead
Qalb E Habib <rana.habib313@gmail.com>
2022-10-24 20:01:09 +0000

    prishma added and logined to render.com

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const port = 8000
+const path = require('path')
 const app = express()
+app.use(express.static("static"));
 
+app.get('/', (req , res ) =>{
 
-
-app.get('/' , (req ,res)=>{
-    res.send('habb from the server')
+    console.log('URL' , req.url , "header" , req.headers, 'Method' , req.method)
+  res.sendFile(path.resolve('./index.html'))
 })
-
-
-app.listen('4000' , ()=>{
-    console.log('server is runnign on the port 4000')
+app.listen(4000 , ()=>{
+    console.log('server running on the port ')
 })
